e620b884af8a3cda6259b9a0ecdada7673984824
Zishan Pathan <141132499+Zishan-Khan01@users.noreply.github.com>
2026-04-26 16:01:05 +0530

    Add files via upload

---
+++ halfPyramid.java
@@ -0,0 +1,19 @@
+public class halfPyramid{
+	public static void halfPyramid(int n){
+		
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=n-i; j++){
+				System.out.print(" ");
+			}
+			for(int j=1; j<=i; j++){
+				System.out.print("*");
+			}
+			//after printing each row print next line
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args){
+		halfPyramid(4);
+	}
+}
