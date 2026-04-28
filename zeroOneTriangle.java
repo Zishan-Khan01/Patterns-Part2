56717283bd605313f6fe25181f0ba68bc34753df
Zishan Pathan <141132499+Zishan-Khan01@users.noreply.github.com>
2026-04-28 19:34:13 +0530

    Add files via upload

---
+++ zeroOneTriangle.java
@@ -0,0 +1,21 @@
+//if  (i+j) is even print 1 else, if (i+j) is odd print 0
+public class zeroOneTriangle{
+
+	public static void zeroOneTriangle(int n){
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=i; j++){
+				if( (i+j) % 2 == 0){
+					System.out.print("1");
+				}else{
+					System.out.print("0");
+				}
+			}
+			//after printing each row print next line
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args){
+		zeroOneTriangle(5);
+	}
+}
